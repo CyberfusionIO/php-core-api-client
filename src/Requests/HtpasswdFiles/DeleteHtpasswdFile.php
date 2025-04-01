51c43b396ddedeb48d404150bfcc915867f767d7
Dick van der Heiden <dick@goedemiddag.nl>
2025-04-01 19:31:40 +0200

    Fix issue where the return type was specified twice

--- src/Requests/HtpasswdFiles/DeleteHtpasswdFile.php
+++ src/Requests/HtpasswdFiles/DeleteHtpasswdFile.php
@@ -33,7 +33,7 @@ class DeleteHtpasswdFile extends Request implements CoreApiRequestContract
      * @throws JsonException
      * @returns DetailMessage|Collection<ValidationError>
      */
-    public function createDtoFromResponse(Response $response): DetailMessage|DetailMessage|Collection
+    public function createDtoFromResponse(Response $response): DetailMessage|Collection
     {
         return DtoBuilder::for($response, DetailMessage::class)->build();
     }
--- src/Requests/MailAccounts/DeleteMailAccount.php
+++ src/Requests/MailAccounts/DeleteMailAccount.php
@@ -35,7 +35,7 @@ class DeleteMailAccount extends Request implements CoreApiRequestContract
      * @throws JsonException
      * @returns DetailMessage|Collection<ValidationError>
      */
-    public function createDtoFromResponse(Response $response): DetailMessage|DetailMessage|Collection
+    public function createDtoFromResponse(Response $response): DetailMessage|Collection
     {
         return DtoBuilder::for($response, DetailMessage::class)->build();
     }
