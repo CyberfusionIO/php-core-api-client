671d2e9fac49aee0b1ebaf145572ac75b5843ebf
Dick van der Heiden <dick@goedemiddag.nl>
2026-05-09 13:54:26 +0200

    Replace removed LinkHeader parser package

--- src/CoreApiPaginator.php
+++ src/CoreApiPaginator.php
@@ -2,7 +2,7 @@
 
 namespace Cyberfusion\CoreApi;
 
-use Graviton\LinkHeaderParser\LinkHeader;
+use Goedemiddag\LinkHeaderParser\LinkHeaderFactory;
 use Illuminate\Support\Collection;
 use Saloon\Http\Request;
 use Saloon\Http\Response;
@@ -16,7 +16,7 @@ class CoreApiPaginator extends PagedPaginator
 
     protected function isLastPage(Response $response): bool
     {
-        return LinkHeader::fromString($response->header('link'))->getRel('next') === null;
+        return LinkHeaderFactory::fromHeader($response->header('link'))->getLink('next') === null;
     }
 
     protected function getPageItems(Response $response, Request $request): array
